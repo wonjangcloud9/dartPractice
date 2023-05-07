3e406cc6c94d49e6975bb4ee0689b963474064bc
wonjangcloud9 <venuscwj@naver.com>
2023-05-07 21:51:08 +0900

    var

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,74 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
+  // hello world
+  print('Hello, World!');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // variable declaration
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  // int
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  int num1 = 10;
+  int num2 = 20;
+  int num3 = num1 + num2;
+  print(num3);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // double
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  double num4 = 10.5;
+  double num5 = 20.5;
+  double num6 = num4 + num5;
+  print(num6);
 
-  final String title;
+  // String
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  String name = 'John';
+  String surname = 'Doe';
+  String fullname = name + ' ' + surname;
+  print(fullname);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // bool
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool isTrue = true;
+  bool isFalse = false;
+  print(isTrue);
+  print(isFalse);
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  // dynamic
+
+  dynamic x = 10;
+  print(x);
+  x = 'Hello';
+  print(x);
+
+  // List
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print(numbers);
+  print(numbers[0]);
+  print(numbers[1]);
+  print(numbers[2]);
+  print(numbers[3]);
+  print(numbers[4]);
+
+  // Map
+
+  Map<String, dynamic> person = {
+    'name': 'John',
+    'surname': 'Doe',
+    'age': 30,
+    'height': 1.80,
+    'isMarried': false,
+    'hobbies': ['reading', 'coding', 'swimming'],
+    'address': {'street': 'Main Street', 'number': 10, 'city': 'London'}
+  };
+  print(person);
+  print(person['name']);
+  print(person['surname']);
+  print(person['age']);
+  print(person['height']);
+  print(person['isMarried']);
+  print(person['hobbies']);
+  print(person['address']);
+  print(person['address']['street']);
+  print(person['address']['number']);
+  print(person['address']['city']);
 }
